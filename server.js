63722dae8b088a8ee96031ff8c900f1bf0114ab2
pavanshyam <65070478+pavanshyam@users.noreply.github.com>
2022-07-17 16:02:35 +0530

    changes for prod-7

--- server.js
+++
@@ -1,7 +0,0 @@
-const express =require('express');
-const path=require('path');
-const app=express();
-app.use(express.static('./dist/elearning'));
-app.get('/*',(req,res)=>res.sendFile('index.html',{root:'dist/elearning'}),
-);
-app.listen(process.env.PORT || 8080);
